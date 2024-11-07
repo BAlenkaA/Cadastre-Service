28a9bc72df9bbecece448fe430856d3f3319fce4
alena <alena@belintsev.ru>
2024-11-07 15:01:20 +0300

    Fix README.md

--- app/api/routers.py
+++ app/api/routers.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter
 
-from app.api.endpoints import query_history_router, user_router
+from app.api.endpoints import query_history_router, user_router, admin_router
 
 main_router = APIRouter()
 main_router.include_router(query_history_router)
 main_router.include_router(user_router)
+main_router.include_router(admin_router)
